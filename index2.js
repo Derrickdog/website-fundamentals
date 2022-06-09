074bfacc8b8062f8494c02a2ac88f5be3ce37fbc
Derrick <derrickryan10@gmail.com>
2022-06-09 10:30:44 -0400

    Add prototypes

--- index2.js
+++ index2.js
@@ -17,3 +17,30 @@ function Book(title, author, numPages, haveRead){
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 console.log(book1.info());
 
+// Prototypes
+function Student(name, grade){
+    this.name = name;
+    this.grade = grade;
+}
+
+Student.prototype.sayName = function() {
+    console.log(this.name);
+}
+Student.prototype.goToProm = function(){
+    console.log("Want to go to prom with me?");
+}
+
+const student1 = new Student("Derrick", 12);
+student1.goToProm();
+
+// Create Prototype
+function SeventhGrader(name) {
+    this.name = name;
+    this.grade = 7;
+}
+
+SeventhGrader.prototype = Object.create(Student.prototype);
+
+const carl = new SeventhGrader("Carl");
+carl.sayName();
+console.log(carl.grade);
