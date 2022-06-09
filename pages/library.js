e09f452a392dd4ca1b2a9649cb528adb6532d3f5
Derrick <derrickryan10@gmail.com>
2022-06-09 11:08:25 -0400

    Add book with prompts

--- pages/library.js
+++ pages/library.js
@@ -9,13 +9,23 @@ function Book(title, author, numPages, haveRead){
 
 Book.prototype.getInfo = function() {
     let haveReadString;
-    (this.haveRead) ? haveReadString = "read" : haveReadString = "not read yet";
+    (this.haveRead.toLowerCase() === "yes") ? haveReadString = "read" : haveReadString = "not read yet";
     return this.title + " by " + this.author + ", " + this.numPages + " pages, " + haveReadString;
 }
 
-// const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+// const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "No");
 // console.log(book1.getInfo());
 
 function addBookToLibrary(){
-    
+    let title = window.prompt("Enter Title");
+    let author = window.prompt("Enter Author");
+    let numPages = window.prompt("Enter Number of Pages");
+    let haveRead = window.prompt("Have you read this book yet? (yes or no)");
+    myLibrary.push(new Book(title, author, numPages, haveRead));
+}
+
+//addBookToLibrary();
+
+for(let book of myLibrary){
+    console.log(book.getInfo());
 }
