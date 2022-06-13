eb487dc7b05c8f6012566dcb0ed92a18a1c46f63
Derrick <derrickryan10@gmail.com>
2022-06-13 10:30:13 -0400

    Add modules

--- index2.js
+++ index2.js
@@ -68,3 +68,70 @@ const FactoryFunction = string => {
   //capitalizeString(); // ERROR!!
   //taco.capitalizeString(); // ERROR!!
   taco.printString(); // this prints "----TACO----"
+
+  // Game
+  const Player = (name, level) => {
+    let health = level * 2;
+    const getLevel = () => level;
+    const getName  = () => name;
+    const die = () => {
+      // uh oh
+    };
+    const damage = x => {
+      health -= x;
+      if (health <= 0) {
+        die();
+      }
+    };
+    const attack = enemy => {
+      if (level < enemy.getLevel()) {
+        damage(1);
+        console.log(`${enemy.getName()} has damaged ${name}`);
+      }
+      if (level >= enemy.getLevel()) {
+        enemy.damage(1);
+        console.log(`${name} has damaged ${enemy.getName()}`);
+      }
+    };
+    return {attack, damage, getLevel, getName};
+  };
+  
+  const jimmie = Player('jim', 10);
+  const badGuy = Player('jeff', 5);
+  jimmie.attack(badGuy);
+
+  // Inheritance
+  const Person = (name) => {
+    const sayName = () => console.log(`my name is ${name}`);
+    return {sayName};
+  }
+  
+  const Nerd = (name) => {
+    // simply create a person and pull out the sayName function with destructuring assignment syntax!
+    const {sayName} = Person(name);
+    const doSomethingNerdy = () => console.log('nerd stuff');
+    return {sayName, doSomethingNerdy};
+  }
+  
+  const jeff = Nerd('jeff');
+  
+  jeff.sayName(); //my name is jeff
+  jeff.doSomethingNerdy(); // nerd stuff
+
+  // Module
+  const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+      add,
+      sub,
+      mul,
+      div,
+    };
+  })();
+  
+  calculator.add(3,5); // 8
+  calculator.sub(6,2); // 4
+  calculator.mul(14,5534); // 77476
